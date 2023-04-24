1d1b34f0beeda36d5b87fda1d994a9b20dbe87a3
cognaiger <100193663+cognaiger@users.noreply.github.com>
2023-04-24 21:18:34 +0700

    first commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,50 @@
+import "./navbar.scss";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { DarkModeContext } from "../../context/darkModeContext";
+
+
+const Navbar = () => {
+
+  const { toggle, darkMode } = useContext(DarkModeContext);
+
+    return (
+        <div className="navbar">
+            <div className="left">
+              <Link to="/" style={{ textDecoration: "none" }}>
+                <span>lamasocial</span>
+              </Link>
+              <HomeOutlinedIcon />
+              {darkMode ? <DarkModeOutlinedIcon onClick={toggle}/> : (
+                <WbSunnyOutlinedIcon onClick={toggle}/>)}
+              <GridViewOutlinedIcon />
+              <div className="search">
+                <SearchOutlinedIcon />
+                <input type="text" placeholder="Search..." />
+              </div>
+            </div>
+            <div className="right">
+              <PersonOutlinedIcon />
+              <EmailOutlinedIcon />
+              <NotificationsOutlinedIcon />
+              <div className="user">
+                <img
+                  src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                  alt=""
+                />
+                <span>Tuan Ngeo</span>
+              </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar
