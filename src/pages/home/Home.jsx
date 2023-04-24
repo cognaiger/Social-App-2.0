dca1e8d217f5563490a23f7f9e6d62c499ea3685
cognaiger <100193663+cognaiger@users.noreply.github.com>
2023-04-24 21:44:13 +0700

    add share

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import Stories from "../../components/stories/Stories";
 import Posts from "../../components/posts/Posts";
+import Shares from "../../components/share/Share";
 import "./home.scss";
 
 const Home = () => {
     return (
         <div className="home">
             <Stories/>
+            <Shares/>
             <Posts/>
         </div>
     )
